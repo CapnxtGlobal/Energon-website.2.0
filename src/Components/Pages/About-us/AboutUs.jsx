1c08ced61d94f91954b369689bcc0c94dd6e5399
padmajakatari <kataripadhu123@gmail.com>
2023-06-05 10:46:11 +0530

    minor changes done

--- src/Components/Pages/About-us/AboutUs.jsx
+++ src/Components/Pages/About-us/AboutUs.jsx
@@ -13,21 +13,22 @@ import "../About-us/About-us.scss";
 import Contact from '../Contact/Contact';
 
 function AboutUs() {
-  let teamCardData=[
+  let teamCardData = [
     {
-      heading:"M.Sripal Reddy",
-      content:"Founder and Chief Executive Officer,"
+      heading: "M.Sripal Reddy",
+      content: "Founder and Chief Executive Officer,"
     }
-  ]
+  ];
+  
   return (
     <>
-    <Navbar/>
-    <main className='Universal-About'>
-    <div className="About">
+      <Navbar />
+      <main className='Universal-About'>
+        <div className="About">
           <div>
             <h1>About Us</h1>
             <p>Established in 2012.Energon has grown into a leading organization in the petro products industry.
-              <br /><br/>
+              <br /><br />
               Energon was founded with the vision of pursuing various oil-related industry and outsourcing activities across India.We bagan with bitumen handling for Hindustan Petroleum Corporation Limited(HPCL)and expanded into drum manufacturing for the Same.Today,our experienced team of over 60 years in business handles
               20,000 MT of black oils and 1,000 KL of lubricants.<br /> <br />
               Our commmitment to innovation and excellence has  positioned Energon as a key player in the petro product sector.</p>
@@ -43,30 +44,30 @@ function AboutUs() {
           <div className='icons'>
             <div className='col'>
               <div className='row'>
-                <img src={safety} alt=''/>
+                <img src={safety} alt='' />
                 <p>Safety</p>
               </div>
               <div className='row'>
-                <img src={quality} alt=''/>
+                <img src={quality} alt='' />
                 <p>Quality</p>
               </div>
               <div className='row'>
-                <img src={integrity} alt=''/>
+                <img src={integrity} alt='' />
                 <p>Integrity</p>
               </div>
             </div>
 
             <div className='col'>
               <div className='row'>
-                <img src={innovation} alt=''/>
+                <img src={innovation} alt='' />
                 <p>Innovation</p>
               </div>
               <div className='row'>
-                <img src={customer} alt=''/>
+                <img src={customer} alt='' />
                 <p>Customer Focus</p>
               </div>
               <div className='row'>
-                <img src={environment} alt=''/>
+                <img src={environment} alt='' />
                 <p>Environmental Responsibility</p>
               </div>
             </div>
@@ -78,7 +79,7 @@ function AboutUs() {
             <h3><p>Energon's services span various</p> <p>locations in India.Explore our interactive</p><p> map to discover our facilities and regional offices.</p></h3>
           </div>
           <div className='map'>
-            <img src={map} alt=''/>
+            <img src={map} alt='' />
           </div>
         </section>
         <section className='team'>
@@ -87,21 +88,21 @@ function AboutUs() {
             <p>Energon's services span various locations in India.Explore our interactive map to discover our facilities and regional offices.</p>
           </div>
           <div className='members'>
-          <div className='sripal'>
-            <h1>M.Sripal Reddy</h1>
-            <p>Founder and Chief Executive Officer</p>
-            <p>M.Sripal Reddy,our esteemed founder and CEO,is an alumnus of the prestigious Indian Institute of Technology(IIT),where he specialized in Chemical Engineering.Sripal's professional journey spans over 15 fruitful years,during which he garnered extensive experience in the petrochemical industry.His deep-rooted passion for the industry is reflected in his hands-on approach and strategic leadership,which has significantly contributed to Energon's growth and success.</p>
-            <p>Sripal's nuanced understanding of the intricate dynamics of the petrochemical sector coupled with his visionary approach had led to the development of innovative strategies that have distinguished Energon in a competitive marketplace.His tenacity,industry knowledge,and commitment to excellence continue to inspire our team and drive the company towards new frontiers of achievement.</p>
-          </div>
-          <div className='image'>
-            <img src={blackimage}alt=''/>
-          </div>
+            <div className='sripal'>
+              <h1>M.Sripal Reddy</h1>
+              <p>Founder and Chief Executive Officer</p>
+              <p>M.Sripal Reddy,our esteemed founder and CEO,is an alumnus of the prestigious Indian Institute of Technology(IIT),where he specialized in Chemical Engineering.Sripal's professional journey spans over 15 fruitful years,during which he garnered extensive experience in the petrochemical industry.His deep-rooted passion for the industry is reflected in his hands-on approach and strategic leadership,which has significantly contributed to Energon's growth and success.</p>
+              <p>Sripal's nuanced understanding of the intricate dynamics of the petrochemical sector coupled with his visionary approach had led to the development of innovative strategies that have distinguished Energon in a competitive marketplace.His tenacity,industry knowledge,and commitment to excellence continue to inspire our team and drive the company towards new frontiers of achievement.</p>
+            </div>
+            <div className='image'>
+              <img src={blackimage} alt='' />
+            </div>
           </div>
           <div className='members'>
             <div className='image'>
-              <img src={blackimage} alt=''/>
+              <img src={blackimage} alt='' />
             </div>
-            <div className='sripal'>
+            <div className='Anand'>
               <h1>Mr.Anand</h1>
               <p>Director and Chief Operations Officer</p>
               <p>Mr.Anand,our esteemed Director and Chief Operations Officer,is a seasoned entrepreneur with a wealth of International business experience.His career,marked by a series of successful ventures across different industries,speaks volumes of his remarkable accumen,strategic foresight,and adaptability.</p>
@@ -110,28 +111,28 @@ function AboutUs() {
             </div>
           </div>
           <div className='members'>
-            <div className='sripal'>
+            <div className='Kishore'>
               <h1>Kishore Chowdary</h1>
               <p>Co-founder and Legal Advisor</p>
               <p>Kishore Chowdary,our co-founder and legal advisor,brings an exceptional legal mind and extensive entrepreneurial experience to the Energon team.A law graduate with a specialization in taxation and corporate laws,Kishore's expertise provides crucial guidance in navigating the complex legal landscape of the petroproduct industry.His profound understanding of the law coupled with his business accumen forms a solid foundation for our business practices,regulatory compliance,and corporate governance.</p>
               <p>In addition to his legal prowess,Kishore has made significant strides in the management of service and retail units.His successful involvement with several startups exhibits his ability to thrive in fast-paced,dynamic environments,traits that are invaluable to Energon's continuous growth and evolution.Kishore's robust legal background and entrepreneurial spirit not only add a unique dimension to our leadership but also contribute significantly to our strategic planning and risk management.</p>
-              </div>
-              <div className='image'>
-                <img src={blackimage} alt=''/>
-              </div>
+            </div>
+            <div className='image'>
+              <img src={blackimage} alt='' />
+            </div>
           </div>
           <div className='members'>
             <div className='imge'>
-              <img src={blackimage} alt=''/>
+              <img src={blackimage} alt='' />
             </div>
             <div className='title'>
               <h1>Title Here</h1>
               <p>Our LPG bottling service the safe and efficient conversion of bulk LPG into easy-to-handle cylinders.We have a dedicated team and advanced equipment to meet the growing demand for LPG across various industries.</p>
             </div>
           </div>
-  </section>
-        <Contact/>
-        </main>
+        </section>
+        <Contact />
+      </main>
     </>
   )
 }
