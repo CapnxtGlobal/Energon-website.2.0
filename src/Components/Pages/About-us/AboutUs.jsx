00bee8d95ac824b46647eddab3152de3b802af53
padmajakatari <kataripadhu123@gmail.com>
2023-06-09 17:43:29 +0530

    added about-us mobile view

--- src/Components/Pages/About-us/AboutUs.jsx
+++ src/Components/Pages/About-us/AboutUs.jsx
@@ -14,14 +14,8 @@ import "../About-us/About-us.scss";
 import Contact from '../Contact/Contact';
 
 function AboutUs() {
-  let teamCardData = [
-    {
-      heading: "M.Sripal Reddy",
-      content: "Founder and Chief Executive Officer,"
-    }
-  ];
-  
-  return (
+
+    return (
     <>
       <Navbar />
       <main className='Universal-About'>
@@ -34,7 +28,7 @@ function AboutUs() {
               20,000 MT of black oils and 1,000 KL of lubricants.<br /> <br />
               Our commmitment to innovation and excellence has  positioned Energon as a key player in the petro product sector.</p>
           </div>
-          <div className="img">
+          <div className="photo">
             <img src={about} alt="" />
           </div>
         </div>
@@ -91,9 +85,9 @@ function AboutUs() {
           <div className='members'>
             <div className='sripal'>
               <h1>M.Sripal Reddy</h1>
-              <p>Founder and Chief Executive Officer</p>
-              <p>M.Sripal Reddy,our esteemed founder and CEO,is an alumnus of the prestigious Indian Institute of Technology(IIT),where he specialized in Chemical Engineering.Sripal's professional journey spans over 15 fruitful years,during which he garnered extensive experience in the petrochemical industry.His deep-rooted passion for the industry is reflected in his hands-on approach and strategic leadership,which has significantly contributed to Energon's growth and success.</p>
-              <p>Sripal's nuanced understanding of the intricate dynamics of the petrochemical sector coupled with his visionary approach had led to the development of innovative strategies that have distinguished Energon in a competitive marketplace.His tenacity,industry knowledge,and commitment to excellence continue to inspire our team and drive the company towards new frontiers of achievement.</p>
+              <p className='founder'>Founder and Chief Executive Officer</p>
+              <p className='Ceo'>M.Sripal Reddy,our esteemed founder and CEO,is an alumnus of the prestigious Indian Institute of Technology(IIT),where he specialized in Chemical Engineering.Sripal's professional journey spans over 15 fruitful years,during which he garnered extensive experience in the petrochemical industry.His deep-rooted passion for the industry is reflected in his hands-on approach and strategic leadership,which has significantly contributed to Energon's growth and success.</p>
+              <p className='esteem'>Sripal's nuanced understanding of the intricate dynamics of the petrochemical sector coupled with his visionary approach had led to the development of innovative strategies that have distinguished Energon in a competitive marketplace.His tenacity,industry knowledge,and commitment to excellence continue to inspire our team and drive the company towards new frontiers of achievement.</p>
             </div>
             <div className='image'>
               <img src={blackimage} alt='' />
@@ -105,18 +99,18 @@ function AboutUs() {
             </div>
             <div className='Anand'>
               <h1>Mr.Anand</h1>
-              <p>Director and Chief Operations Officer</p>
-              <p>Mr.Anand,our esteemed Director and Chief Operations Officer,is a seasoned entrepreneur with a wealth of International business experience.His career,marked by a series of successful ventures across different industries,speaks volumes of his remarkable accumen,strategic foresight,and adaptability.</p>
-              <p>Anand's global exposure,derived from extensive travel and diverse business engagements,gives him a unique perspective on market dynamics,cultural nuances,and operational strategies.This international outlook has been instrumental in fostering Energon's global connections and broadening our operational horizons.</p>
-              <p>As a leader,Anand's multi-faceted business expertise and relentless pursuit of excellence guide our operational efficiency,ensuring that Energon consistently delivers the highest standrads of services.His entrepreneurial spirit fuels innvation within our team and continues to drive Energon towards greater heights in the petrochemical industry.</p>
+              <p className='director'>Director and Chief Operations Officer</p>
+              <p className='chief'>Mr.Anand,our esteemed Director and Chief Operations Officer,is a seasoned entrepreneur with a wealth of International business experience.His career,marked by a series of successful ventures across different industries,speaks volumes of his remarkable accumen,strategic foresight,and adaptability.</p>
+              <p className='global'>Anand's global exposure,derived from extensive travel and diverse business engagements,gives him a unique perspective on market dynamics,cultural nuances,and operational strategies.This international outlook has been instrumental in fostering Energon's global connections and broadening our operational horizons.</p>
+              <p className='leader'>As a leader,Anand's multi-faceted business expertise and relentless pursuit of excellence guide our operational efficiency,ensuring that Energon consistently delivers the highest standrads of services.His entrepreneurial spirit fuels innvation within our team and continues to drive Energon towards greater heights in the petrochemical industry.</p>
             </div>
           </div>
           <div className='members'>
             <div className='Kishore'>
               <h1>Kishore Chowdary</h1>
-              <p>Co-founder and Legal Advisor</p>
-              <p>Kishore Chowdary,our co-founder and legal advisor,brings an exceptional legal mind and extensive entrepreneurial experience to the Energon team.A law graduate with a specialization in taxation and corporate laws,Kishore's expertise provides crucial guidance in navigating the complex legal landscape of the petroproduct industry.His profound understanding of the law coupled with his business accumen forms a solid foundation for our business practices,regulatory compliance,and corporate governance.</p>
-              <p>In addition to his legal prowess,Kishore has made significant strides in the management of service and retail units.His successful involvement with several startups exhibits his ability to thrive in fast-paced,dynamic environments,traits that are invaluable to Energon's continuous growth and evolution.Kishore's robust legal background and entrepreneurial spirit not only add a unique dimension to our leadership but also contribute significantly to our strategic planning and risk management.</p>
+              <p className='founder'>Co-founder and Legal Advisor</p>
+              <p className='Ceo'>Kishore Chowdary,our co-founder and legal advisor,brings an exceptional legal mind and extensive entrepreneurial experience to the Energon team.A law graduate with a specialization in taxation and corporate laws,Kishore's expertise provides crucial guidance in navigating the complex legal landscape of the petroproduct industry.His profound understanding of the law coupled with his business accumen forms a solid foundation for our business practices,regulatory compliance,and corporate governance.</p>
+              <p className='esteem'>In addition to his legal prowess,Kishore has made significant strides in the management of service and retail units.His successful involvement with several startups exhibits his ability to thrive in fast-paced,dynamic environments,traits that are invaluable to Energon's continuous growth and evolution.Kishore's robust legal background and entrepreneurial spirit not only add a unique dimension to our leadership but also contribute significantly to our strategic planning and risk management.</p>
             </div>
             <div className='imag'>
               <img src={blackimage} alt='' />
